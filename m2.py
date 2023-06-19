9bdb6521d78c9e36d7db2c18f1302534cfbdb759
free-proger <free-proger@yandex.ru>
2023-06-19 19:11:58 +0300

    add

---
+++ m2.py
@@ -0,0 +1,19 @@
+def even(a, b, c):
+    f = False
+    if a % 2 == 0:
+        f = True
+    if b % 2 == 0:
+        f = True
+    if c % 2 == 0:
+        f = True
+    return f
+if __name__ == "__main__":
+    a, b, c = map(int, input().split())
+    f = False
+    if a % 2 == 0:
+        f = True
+    if b % 2 == 0:
+        f = True
+    if c % 2 == 0:
+        f = True
+    print(f)
---
+++ m3.py
@@ -0,0 +1,7 @@
+#!/usr/env python3
+# -*- coding: utf-8 -*-
+if __name__ == "__main__":
+    for i in range(1,10):
+        for j in range(1,10):
+            print(f'{i} * {j} = {i*j}')
+    print('----------')
